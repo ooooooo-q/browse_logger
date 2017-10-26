aeab5ab4d3fd6a7fd8a8527ec3400965a5e14138
ooooooo-q <ooooooo-q@users.noreply.github.com>
2017-10-26 23:40:02 +0900

    イベントの順序を考慮して記録を行うように

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,76 +1,119 @@
 let db;
 let user;
-let currentUrl;
+
 let stockUrl;
 let storage;
 
-chrome.tabs.onActivated.addListener(() => {
+let lastActiveTabId = null;
+let lastActiveUrl = null;
+let lastDocRef = null;
+let lastActivatedTime = null;
+
+
+const getLastTab = (cb) => {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
     if (tabs.length > 0) {
-      log("onActivated", tabs[0]);
+      cb(tabs[0])
     }
   });
-});
+};
 
-chrome.tabs.onUpdated.addListener(() => {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
-    if (tabs.length > 0) {
-      log("onUpdated", tabs[0]);
-    }
+const getScreenShotUrl = (cb) => {
+  chrome.tabs.captureVisibleTab(function(screenShotUrl) {
+    cb(screenShotUrl)
   });
-});
+};
+
+const console = chrome.extension.getBackgroundPage().console;
+
+chrome.tabs.onActivated.addListener(() => getLastTab((tab) => loggedUrl(tab)));
+chrome.tabs.onUpdated.addListener(() => getLastTab((tab) => loggedUrl(tab)));
+chrome.tabs.onRemoved.addListener((id) => onRemove(id));
 
 
-function capture() {
-  if (!currentUrl){return}
 
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
-    if (tabs[0] && tabs[0].url === currentUrl) {
-      chrome.tabs.captureVisibleTab(function(screenShotUrl) {
+function loggedUrl(tab) {
 
-        const console = chrome.extension.getBackgroundPage().console;
-        const base64String = screenShotUrl.split("base64,")[1];
-        const fileKey = `${Date.now()}.jpg`
-
-        const metadata = {
-          contentType: 'image/jpeg',
-        };
-
-        var storageRef = firebase.storage().ref(fileKey);
-
-        storageRef.putString(base64String, 'base64', metadata).then(function(snapshot) {
-          console.log('Uploaded a base64 string!', snapshot);
-        }, (e) => {
-          console.log(e);
-        });
-      });
-    }
-  });
-}
-
-
-function log(status, tab) {
-
-  if(!user) {return};
+  if (!user) {return;}
 
   const url = tab.url;
   const title = tab.title;
   const timestamp = Date.now();
   const uid = user.uid;
 
-  const console = chrome.extension.getBackgroundPage().console;
+  // 変更がなかったら記録しない
+  if (lastActiveUrl === url){return;}
+
+  lastActiveTabId = tab.id;
+  lastActiveUrl = url;
 
   console.log(url);
 
   const collection = `/log/${uid}/raw`;
 
-  db.collection(collection).add({uid, url, title, status, timestamp})
-  .then((docRef) => {console.log("Document written with ID: ", docRef.id)})
+  if (lastDocRef) {
+    lastDocRef.update({"duration": timestamp - lastActivatedTime})
+      .then(() => {
+        console.log("Document update with ID: ")
+      })
+      .catch((error) => {console.log("Error update document: ", error)})
+  }
+
+  lastActivatedTime = timestamp;
+
+  db.collection(collection).add({uid, url, title, timestamp})
+  .then((docRef) => {
+    console.log("Document written with ID: ", docRef.id)
+    lastDocRef = docRef;
+  })
   .catch((error) => {console.log("Error adding document: ", error)})
 
-  currentUrl = url;
   //setTimeout(capture, 10 * 1000) ライフサイクルを考えてから
 }
+
+const onRemove = (id) => {
+  if (id === lastActiveTabId) {
+
+    if (lastDocRef) {
+      const timestamp = Date.now();
+      lastDocRef.update({"duration": timestamp - lastActivatedTime})
+        .then(() => {
+          console.log("Document update with ID: ")
+        })
+        .catch((error) => {console.log("Error update document: ", error)})
+    }
+
+    lastActiveTabId = null;
+    lastActiveUrl = null;
+    lastDocRef = null;
+  }
+};
+
+
+function capture() {
+  if (!lastActiveUrl){return}
+
+  getLastTab((tab) => {
+    if (tab.url !== lastActiveUrl) { return;}
+
+    getScreenShotUrl((screenShotUrl) => {
+
+      const base64String = screenShotUrl.split("base64,")[1];
+      const fileKey = `${Date.now()}.jpg`
+
+      const metadata = {
+        contentType: 'image/jpeg',
+      };
+
+      var storageRef = firebase.storage().ref(fileKey);
+
+      storageRef.putString(base64String, 'base64', metadata)
+        .then((snapshot) => {console.log('Uploaded a base64 string!', snapshot)}
+          , (e) => {console.log(e);});
+    });
+  });
+}
+
 
 
 function initApp(){
